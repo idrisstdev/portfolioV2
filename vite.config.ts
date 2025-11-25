e586f28e5ff9de18d5d7665f3c3f5e4176c3b0ba
idrisstdev <idriss.tameri@gmail.com>
2025-11-25 23:38:24 +0100

    fix dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,8 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig, PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 const plugins: PluginOption[] = [vue()]
-
-if (process.env.NODE_ENV === 'development') {
-  plugins.push(vueDevTools())
-}
 
 // https://vite.dev/config/
 export default defineConfig({
