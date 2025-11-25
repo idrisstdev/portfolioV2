6d76a53050015abfaafbfcfba5af13c77d4278cf
idrisstdev <idriss.tameri@gmail.com>
2025-11-25 22:55:39 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
+const plugins: PluginOption[] = [vue()]
+
+if (process.env.NODE_ENV === 'development') {
+  plugins.push(vueDevTools())
+}
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/portfolioV2/',
-  plugins: [vue(), vueDevTools()],
+  plugins,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
